a8866884db8be36e5c27376fa5096f839ab017ac
senthilkumar123456 <141622183+senthilkumar123456@users.noreply.github.com>
2023-08-22 12:40:25 +0530

    Add files via upload

---
+++ if else2.py
@@ -0,0 +1,78 @@
+##task5
+##num=int(input('enter your number'))
+##if(num>13):
+##    if(num==1):
+##        print('1 for januaryb and number of days 31')
+##elif(num==2):
+##    print('2 for february and number of days 28')
+##elif(num==3):
+##    print('3 for march and number of days 31')
+##elif(num==4):
+##    print('4 for april and number of days 30')
+##elif(num==5):
+##    print('5 for may and number of dars 31')
+##elif(num==6):
+##    print('6 for june and mumber of days 30')
+##elif(num==7):
+##    print('7 for july and number of days 31')
+##elif(num==8):
+##    print('8 for august and number of days 31')
+##elif(num==9):
+##    print('9 for september and number of days 30')
+##elif(num==10):
+##    print('10 for october and number of days 31')
+##elif(num==11):
+##    print('11 for november and number of days 30')
+##elif(num==12):
+##    print('12 for december and number of days 31')
+##else:
+##    print('enter the valued number')
+##task6
+##a=int(input('enter the value of a:'))
+##b=int(input('enter the value of b:'))
+##c=int(input('enter the value of c:'))
+##if(a==b)and(a==c):
+##    print('it is equilateral triangle')
+##elif(a!=b)or(a!=c)
+##    print('it is scalent triangle')
+##elif(a==b)and(a==c):
+##    print('it is isosceles triangle')
+##task7
+##unit=int(input('enter your electric bill'))
+##if(unit>100):
+##    if(unit>100)and (unit<=200):
+##        amt=(unit-100)*5
+##        print('your electric bill rs',amt)
+##elif(unit>200):
+##    amt2=(unit-100)*10
+##    print('your electric bill rs',amt)
+##else:
+##    print('you have no chages')
+##task8
+##year=int(input('enter your year'))
+##if(year%4==0):
+##    if(year%200):
+##        if(year%400):
+##            print('this is a leap year')
+##        else:
+##            print('this is a not leap year')
+##    else:
+##        print('this is a leap year')
+##else:
+##    print('this is a not leap year')
+##ttask9
+##num1=float(input('enter the value:'))
+##num2=float(input('enter the value:'))
+##operator=input('enter the operator symbols(+,-,*,/,):')
+##if(operator=='+'):
+##    print('value is',num1+num2)
+##elif(operator=='-'):
+##    print('value is',num1-num2)
+##elif(operator=='*'):
+##    print('value is',num1*num2)
+##elif(operator=='/'):
+##    print('value is',num1/num2)
+##else:
+##    print('enter the value operator.')
+
+
